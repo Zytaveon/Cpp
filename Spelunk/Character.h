d77a64ffd4b1199ca6bbf7b4c6772c73afabb691
Zytaveon <35542901+Zytaveon@users.noreply.github.com>
2025-03-04 16:26:29 -0600

    Added Character Class
    Added Character class that Player and Monster will inherit from. Also added a level legend text file because I want everything to be in the text file and get created when the level is loaded, including the player and all the monsters/enemies. I feel like I am in over my head but one little step at a time.

---
+++ Spelunk/Character.h
@@ -0,0 +1,33 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "include/raylib.h";
+
+class Character{
+    public:
+        //Constructor
+        Character();
+
+        void updateCharacter();
+        Vector2 getPosition();
+
+    private:
+
+        Vector2 position;
+
+};
+
+class Player : public Character{
+    public:
+
+    private:
+};
+
+class Monster : public Character{
+    public:
+
+    private:
+
+};
+
+#endif
