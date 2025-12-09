f7ec202f6742d073ee6ed842d03c57a2bc7381a8
Zytaveon <35542901+Zytaveon@users.noreply.github.com>
2025-12-08 20:58:55 -0600

    AOC Catch up
    Have a few Advent of Code things not pushed so catching up

--- AOC2015/Day2/Code.cpp
+++ AOC2015/Day2/Code.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 int main(void){
 
-    std::cout << "Hello There" << std::endl;
+    std::string testInput = "2x3x4";
+
+    std::vector<std::string> sides = split(testInput, 'x');
 
     return 0;
 }
---
+++ AOC2025/1/code.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int maid(void){
+
+    std::cout << "Hello World" << std::endl;
+
+    return 0;
+}
